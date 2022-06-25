f61fd170a5aee391695814c3bbba1be9b84be553
mddiaz2 <mddiaz2511@gmail.com>
2022-06-25 15:38:54 -0500

    carga

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require("cors");
+const morgan = require('morgan');
+const app= express();
+
+const port= process.env.PORT || 3000
+
+//middlewars
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
